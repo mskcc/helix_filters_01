4d005778a28b6d02530395fb36e3f3438aceeb4b
Stephen Kelly <stevekm@users.noreply.github.com>
2020-05-18 19:06:56 -0400

    add workflow test for two maf files

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -81,3 +81,74 @@ class TestWorkflow(unittest.TestCase):
                 }
 
             self.assertDictEqual(output_json, expected_output)
+
+    def test_run_worflow_two_mafs(self):
+        """
+        Test that the workflow works correctly when run with a
+        """
+        input_json_file = os.path.join(DATA_SETS['Proj_08390_G']['DIR'], "input.all.json")
+        with TemporaryDirectory() as tmpdir:
+
+            output_dir = os.path.join(tmpdir, "output")
+            tmp_dir = os.path.join(tmpdir, "tmp")
+            cache_dir = os.path.join(tmpdir, "cache")
+
+            command = [
+                "cwl-runner",
+                *CWL_ARGS,
+                "--outdir", output_dir,
+                "--tmpdir-prefix", tmp_dir,
+                "--cachedir", cache_dir,
+                cwl_file, input_json_file
+                ]
+
+            returncode, proc_stdout, proc_stderr = run_command(command)
+
+            output_json = json.loads(proc_stdout)
+
+            self.assertEqual(returncode, 0)
+
+            expected_output = {
+                "analysis_dir": {
+                    "class": "Directory",
+                    "basename": "analysis",
+                    "listing": [
+                        {
+                            "location": 'file://' + os.path.join(output_dir, "analysis/Proj_08390_G.muts.maf"),
+                            "basename": "Proj_08390_G.muts.maf",
+                            "class": "File",
+                            "checksum": "sha1$5b2f8277269026d2b7ea0edc4455cc97c7f68523",
+                            "size": 46695,
+                            "path": os.path.join(output_dir, "analysis/Proj_08390_G.muts.maf")
+                        },
+                        {
+                            "location": 'file://' + os.path.join(output_dir, 'analysis/Proj_08390_G.gene.cna.txt'),
+                            "basename": "Proj_08390_G.gene.cna.txt",
+                            "class": "File",
+                            "checksum": "sha1$ab17d587ad5ae0a87fd6c6d4dd2d5d1701208ce9",
+                            "size": 173982,
+                            "path": os.path.join(output_dir, "analysis/Proj_08390_G.gene.cna.txt")
+                        }
+                    ],
+                    "location": 'file://' + os.path.join(output_dir, "analysis"),
+                    "path": os.path.join(output_dir, "analysis")
+                },
+                "portal_dir": {
+                    "class": "Directory",
+                    "basename": "portal",
+                    "listing": [
+                        {
+                            "location": 'file://' + os.path.join(output_dir, "portal/data_CNA.txt"),
+                            "basename": "data_CNA.txt",
+                            "class": "File",
+                            "checksum": "sha1$ab17d587ad5ae0a87fd6c6d4dd2d5d1701208ce9",
+                            "size": 173982,
+                            "path": os.path.join(output_dir, "portal/data_CNA.txt")
+                        }
+                    ],
+                    "location": 'file://' + os.path.join(output_dir, "portal"),
+                    "path": os.path.join(output_dir, "portal")
+                }
+            }
+            self.maxDiff = None
+            self.assertDictEqual(output_json, expected_output)
