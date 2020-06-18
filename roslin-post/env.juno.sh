f460ed061bb85b49a4c03797d806e6b1b42cf4f4
Stephen Kelly <stevekm@users.noreply.github.com>
2020-06-18 08:13:22 -0400

    remove roslin-post Nextflow workflow

--- roslin-post/env.juno.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# top-level environment needed to run Nextflow on Juno HPC
-module unload java && module load java/jdk1.8.0_202
-module unload singularity && module load singularity/3.1.1
