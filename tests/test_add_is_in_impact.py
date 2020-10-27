f662a8ae073f1ca89b383f9bf10d746ab2a50b34
Stephen Kelly <stevekm@users.noreply.github.com>
2020-10-27 15:45:34 -0400

    add test case for full sized maf file

--- tests/test_add_is_in_impact.py
+++ tests/test_add_is_in_impact.py
@@ -21,6 +21,7 @@ class TestInImpactScript(unittest.TestCase):
     def test_is_in_impact_0(self):
         """
         Test that maf entries are flagged if they are in the IMPACT set
+        This test case handles the usage of an IMPACT list that lacks some chroms in the maf file
         """
         maf_lines = [
             ['Chromosome', 'Start_Position'],
@@ -55,6 +56,9 @@ class TestInImpactScript(unittest.TestCase):
             self.assertEqual(mutations, expected_mutations)
 
     def test_is_in_impact_1(self):
+        """
+        Test case with a larger IMPACT list that includes all chrom in the maf file
+        """
         maf_lines = [
             ['Chromosome', 'Start_Position'],
             ['1', '100'],
@@ -82,6 +86,35 @@ class TestInImpactScript(unittest.TestCase):
                 {'Chromosome': '3', 'Start_Position': '300', 'is_in_impact': 'False'}]
 
             self.assertEqual(mutations, expected_mutations)
+
+    def test_is_in_impact_3(self):
+        """
+        Test IMPACT script with full size maf file
+        """
+        input_maf_file = os.path.join(DATA_SETS['Proj_08390_G']['MAF_FILTER_DIR'], 'Sample1', 'Sample1.Sample2.muts.maf')
+        impact_lines = [
+            ['10', '100945885', '100945888'], # chrom, start, stop
+            ['X', '9577377', '9577379'],
+            ['9', '87476723', '87476725'],
+        ]
+        with TemporaryDirectory() as tmpdir:
+            output_file = os.path.join(tmpdir, "output.txt")
+            impact_file = write_table(tmpdir = tmpdir, filename = 'IMPACT.txt', lines = impact_lines)
+            command = [ impact_script, '--input_file', input_maf_file, '--output_file', output_file, '--IMPACT_file', impact_file ]
+            returncode, proc_stdout, proc_stderr = run_command(command, testcase = self, validate = True)
+
+            # validate output mutations
+            comments, mutations = load_mutations(output_file)
+            self.assertEqual(len(mutations), 12514)
+            num_True = 0
+            num_False = 0
+            for mut in mutations:
+                if mut['is_in_impact'] == 'True':
+                    num_True += 1
+                elif mut['is_in_impact'] == 'False':
+                    num_False += 1
+            self.assertEqual(num_True, 3)
+            self.assertEqual(num_False, 12511)
 
 
 # run the test suite from the command line
