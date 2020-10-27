b8277477a115455eac08fd2922d113e8ad298852
svural <suleymanvural@gmail.com>
2020-10-27 08:01:56 -0400

    formated and cleaned up add_is_in_impact.py

--- bin/add_is_in_impact.py
+++ bin/add_is_in_impact.py
@@ -1,47 +1,75 @@
+#!/usr/bin/env python3
+import argparse,csv
+from cBioPortal_utils import parse_header_comments
 
-import time
-# import pickle
+def load_IMPACT_data(filename):
+    d={}
+    with open(filename,'r') as f:#
+        for line in f:
+            line=line.rstrip().split()
+            chr=line[0]
+            spos=int(line[1])
+            epos=int(line[2])
+            range_l=range(spos,epos+1)
+            if chr not in d:
+                d[chr]=[]
+            d[chr]+=range_l
+    return d
 
-t0=time.time()
+def is_in_IMPACT(chr,pos,IMPACT_d):
+    return str(pos in IMPACT_d[chr])
 
-f=open('IMPACT_taarget_files/IMPACT341_b37_targets.bed.tsv')
-d={}
-for line in f:
-    line=line.rstrip().split()
-    chr=line[0]
-    spos=int(line[1])
-    epos=int(line[2])
+def parse_CLI_args():
+    """
+    Parse the CLI args
+    add_is_in_impact.py Proj_08390_G.muts.maf IMPACT468_b37_targets.bed
+    """
+    parser = argparse.ArgumentParser(description = 'Script for adding if mutation is in IMPACT panel')
+    parser.add_argument('--input_file',    dest = 'input_file',          required = True,                     help='Input maf filename')
+    parser.add_argument('--output_file',   dest = 'output_file',         required = False, default='default', help='Output maf filename')
+    parser.add_argument('--IMPACT_file',   dest = 'IMPACT_target_files', required = True,                     help='IMPACT file to use')
 
-    range_l=range(spos,epos+1)
+    args = parser.parse_args()
 
-    if chr not in d:
-        d[chr]=[]
+    if args.output_file=='default':
+        args.output_file=args.input_file.split('/')[-1].split('.')[0]+'_is_in_IMPACT_added.maf'
 
-    d[chr]+=range_l
+    return args
 
 
-#pickle.dump( d, open( "save.p", "wb" ) )
-# d = pickle.load( open( "save.p", "rb" ) )
+def main():
+    args=parse_CLI_args()
 
-f=open('maf/s_C_99C69R_P001_d.FFPEPOOLEDNORMAL_JAX_0454.svs.pass.vep.maf')
-f.readline()
-header=f.readline()
-for line in f:
-    line=line.rstrip().split()
+    IMPACT_d=load_IMPACT_data(args.IMPACT_target_files)
 
-    chr='1' #line[4]
-    pos=2488103 #line[5]
+    # get the comments from the file and find the beginning of the table header
+    comments, start_line = parse_header_comments(args.input_file)
+    comments_lines = [ c + '\n' for c in comments ]
 
-    print chr,pos
+    is_in_impact_added_output=[]
+    with open(args.input_file,'r') as fin:
+        # skip comment lines
+        while start_line > 0:
+            next(fin)
+            start_line -= 1
 
-    if int(pos) in d[chr]:
-        print 'in'
-    else:
-        print 'out'
+        reader = csv.DictReader(fin, delimiter = '\t')
+        fieldnames = reader.fieldnames
+        fieldnames.append('Is_in_IMPACT')
+        for row in reader:
+            row['Is_in_IMPACT']=is_in_IMPACT(row['Chromosome'],row['Start_Position'],IMPACT_d)
+            is_in_impact_added_output.append(row)
 
-    break
+    # write analysis files
+    with open(args.output_file,'w') as fout:
+        fout.writelines(comments_lines)
+        # ignore fields not in fieldnames
+        # NOTE: csv writer includes carriage returns that we dont want
+        # https://stackoverflow.com/questions/3191528/csv-in-python-adding-an-extra-carriage-return-on-windows
+        writer = csv.DictWriter(fout, delimiter = '\t', fieldnames = fieldnames, extrasaction = 'ignore', lineterminator='\n')
+        writer.writeheader()
+        for row in is_in_impact_added_output:
+            writer.writerow(row)
 
-t1=time.time()
-print(t1-t0)
-
-# print('done')
+if __name__ == '__main__':
+    main()
