222cba02ee247190214b34aa9650ecef32d9de6f
Stephen Kelly <stevekm@users.noreply.github.com>
2021-01-06 09:59:46 -0500

    add TMB calculation from maf file

--- bin/calc-tmb.py
+++ bin/calc-tmb.py
@@ -7,11 +7,24 @@ num_variants / bases_covered * 1,000,000 = TMB in Megabases
 """
 import sys
 import argparse
+from cBioPortal_utils import MafReader
 
-def calc_from_values(num_variants, genome_coverage, megabases = False, _print = False, func = None):
+def calc_from_values(
+    num_variants, # str | float | int
+    genome_coverage, # str | float | int
+    megabases = False,
+    _print = False,
+    output_file = None,
+    func = None # dummy arg for parser
+    ):
     """
+    Simple calculation of TMB tumor mutation burden.
+
+    Num variants / bases covered
+
+    Report the result in variants/Megabases unless otherwise specified
     """
-    # print(num_variants, genome_coverage, megabases, _print)
+
     num_variants = int(num_variants)
     genome_coverage = int(genome_coverage)
 
@@ -34,7 +47,35 @@ def calc_from_values(num_variants, genome_coverage, megabases = False, _print = 
         import numpy
         print(numpy.format_float_positional(tmb))
 
+    if output_file:
+        import numpy
+        with open(output_file, "w") as f:
+            f.write(numpy.format_float_positional(tmb) + '\n')
+
     return(tmb)
+
+def calc_from_file(
+    input_file,
+    output_file,
+    genome_coverage, # str | float | int
+    func = None # dummy arg for parser
+    ):
+    """
+    Wrapper around TMB calc function to use for reading variants from file
+    """
+    # use some default values
+    megabases = True
+    _print = False
+    # reader for variants in the file
+    maf_reader = MafReader(input_file)
+    num_variants = maf_reader.count()
+    calc_from_values(
+        num_variants = num_variants,
+        output_file = output_file,
+        genome_coverage = genome_coverage,
+        megabases = megabases,
+        _print = _print
+        )
 
 def parse():
     """
@@ -46,12 +87,22 @@ def parse():
     # add sub-parsers for specific file outputs
     subparsers = parser.add_subparsers(help ='Sub-commands available')
 
+    # parser to calculate TMB from raw values passed on the CLI
     from_values = subparsers.add_parser('from-values', help = 'Calculate the TMB in Megabases from values passed')
     from_values.add_argument('--num-variants', dest = 'num_variants', required = True, help = 'Number of variants')
     from_values.add_argument('--genome-coverage', dest = 'genome_coverage', required = True, help = 'Number of base pairs of the genome covered by the assay')
     from_values.add_argument('--raw', dest = 'megabases', action='store_false', help = 'Return the raw TMB value instead of the value in Megabases')
     from_values.add_argument('--no-print', dest = '_print', action='store_false', help = 'Dont print the output')
+    from_values.add_argument('--output-file', dest = 'output_file', help = 'File to write values to')
     from_values.set_defaults(func = calc_from_values)
+
+    # parser to calculate TMB from a variant file passed
+    from_file = subparsers.add_parser('from-file', help = 'Calculate the TMB in Megabases from a variant file')
+    from_file.add_argument('input_file', help = 'File to read variants from')
+    from_file.add_argument('output_file', help = 'File to write TMB value to')
+    from_file.add_argument('--genome-coverage', dest = 'genome_coverage', required = True, help = 'Number of base pairs of the genome covered by the assay')
+    from_file.set_defaults(func = calc_from_file)
+
 
     args = parser.parse_args()
     args.func(**vars(args))
