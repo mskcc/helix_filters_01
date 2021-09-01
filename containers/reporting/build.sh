57033f5eb2186d734c7ea6e908fb65db47ac9a48
Stephen Kelly <stevekm@users.noreply.github.com>
2021-08-31 20:58:30 -0400

    start container for compiling reports

---
+++ containers/reporting/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# script to build the R container because it needs the scripts from the bin dir
+[ -e report ] && rm -rf report
+cp -r ../../report .
+docker build -t "mskcc/helix_filters_01:reporting" .
