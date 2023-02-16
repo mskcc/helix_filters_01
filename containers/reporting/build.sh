8392198695df40d356a37dd0be932116ec136514
svural <suleymanvural@gmail.com>
2023-02-16 11:09:22 -0500

    clean up unnecessary things

--- containers/reporting/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# script to build the R container because it needs the scripts from the bin dir
-[ -e report ] && rm -rf report
-cp -r ../../report .
-docker build -t "mskcc/helix_filters_01:reporting" .
