5162a61a3b232efc12b3e117e5c651864cca2444
svural <suleymanvural@gmail.com>
2023-03-22 11:45:21 -0400

    deleted temp files

--- report/temp_to_cleanup/command.sh
+++
@@ -1,1 +0,0 @@
-Rscript compile_sample_level.R --project_path data/argos/11704_Y/1.1.2/20221102_20_40_060423 --geneAnnotation_path data/geneAnnotation.rds
--- report/temp_to_cleanup/test.sh
+++
@@ -1,4 +0,0 @@
-IFS=$'\n' read -d '' -r -a lines < ../../sample_project_paths
-
-# all lines
-echo "${lines[@]}"
